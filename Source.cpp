6d2f4da6c723dbe16aa94a7ebd6151c01dfd21a2
Rogelio Negrete <rogelio_negrete@live.com>
2016-09-02 13:51:01 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -6,18 +6,20 @@
 #include <string>
 #include <unordered_map>
 #include <stack>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
-int eval_operation(char operation, int operand1, int operand2)
+int eval_operation(string operation, int operand1, int operand2)
 {
-	if (operation == '+')
+	if (operation == "+")
 		return operand1 + operand2;
-	else if (operation == '-')
+	else if (operation == "-")
 		return operand1 - operand2;
-	else if (operation == '*')
+	else if (operation == "*")
 		return operand1 * operand2;
-	else if (operation == '/')
+	else if (operation == "/")
 		return operand1 / operand2;
 	else
 	{
@@ -26,59 +28,63 @@ int eval_operation(char operation, int operand1, int operand2)
 	}
 }
 
-void process_expression(string expression)
+vector<string> split(const string &s, char delim) {
+	vector<string> elems;
+	stringstream ss;
+	ss.str(s);
+	string item;
+	while (getline(ss, item, delim)) {
+		elems.push_back(item);
+	}
+	return elems;
+}
+
+void process_expression(vector<string> expression)
 {
 	unordered_map<string, int> dict;
-	stack<int> resultlist;
-	string word = "";
+	stack<int> stack;
 
-	cout << "Expression to process: " << expression << endl;
-
-	for (int i = 0; i < expression.length(); i++)
+	for (int i = 0; i < expression.size(); i++)
 	{
 		// check for operations
-		if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/')
+		if (expression.at(i) == "+" || expression.at(i) == "-" || expression.at(i) == "*" || expression.at(i) == "/")
 		{
-			int operand1 = resultlist.top(); resultlist.pop();
-			int operand2 = resultlist.top(); resultlist.pop();
+			int operand1 = stack.top(); stack.pop();
+			int operand2 = stack.top(); stack.pop();
 
 			// push computed value back onto stack
-			resultlist.push(eval_operation(expression[i], operand1, operand2));
+			stack.push(eval_operation(expression.at(i), operand1, operand2));
 
 		}
-		// found to be a char[a-z]
-		else if (expression[i] >= 'a' && expression[i] <= 'z')
+		// found to be a number
+		else if (expression.at(i) >= "0" && expression.at(i) <= "9")
 		{
-			word += expression[i];
+			stack.push(stoi(expression.at(i)));
 		}
-		else if (isspace(expression[i]))
+		// base case - there should only be one value left in the stack which is the final value
+		else if (expression.at(i) == "$" && !stack.empty())
 		{
-
+			cout << "Final Value: " << stack.top() << endl;
+		}
+		else // word or char
+		{
 			// check if current element is NOT in our dict
-			if (dict.count(word) == 0)
+			if (dict.count(expression.at(i)) == 0)
 			{
 				char char_value;
 				int int_value;
-				// Prompt user for the value of "expression[i]"
-				cout << "\nEnter the value of " << word << ": ";
+				// Prompt user for the value of "expression.at(i)"
+				cout << "\nEnter the value of " << expression.at(i) << ": ";
 				cin >> char_value;
 				int_value = char_value - '0'; // quick way to convert from char to int			
-				resultlist.push(int_value); // push int_value to the stack 
-				dict.emplace(word, int_value); // store into our dict for future use
+				stack.push(int_value); // push int_value to the stack 
+				dict.emplace(expression.at(i), int_value); // store into our dict for future use
 			}
 			else // current element is in our dict
 			{
-
-				int int_value = dict.find(word)->second; // find our match in the dict
-				resultlist.push(int_value); // push int_value to the stack 
+				int int_value = dict.find(expression.at(i))->second; // find our match in the dict
+				stack.push(int_value); // push int_value to the stack 
 			}
-
-			word.clear(); // clear out word 
-		}
-		// base case - there should only be one value left in the stack which is the final value
-		else if (expression[i] == '$' && !resultlist.empty())
-		{
-			cout << "Final Value: " << resultlist.top() << endl;
 		}
 
 	}
@@ -100,16 +106,16 @@ void repeat_prompt()
 	}
 }
 
+
 int main() {
-	
+	cout << "Version with VECTOR" << endl;
 	while (true)
 	{
 		string postfix_expression;
-		cout << "Enter a postfix expression with a $ at the end: ";
+		cout << "\nEnter a postfix expression with a $ at the end: ";
 		getline(cin, postfix_expression);
-		process_expression(postfix_expression);
+		process_expression(split(postfix_expression, ' '));
 		repeat_prompt();
 		cin.ignore(1000, '\n');
 	}
-	
 }
